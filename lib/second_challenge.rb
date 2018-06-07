644ac7999213e3b11fb48172e77265c71afdb69d
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-07 00:04:08 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,15 +1,15 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-        :patriarch => {}
-        :matriarch => {}
-        :hero => {}
+        :patriarch => {},
+        :matriarch => {},
+        :hero => {},
         :hero_friends => []
       }, 
     :capulet => {
-        :patriarch => {}
-        :matriarch => {}
-        :hero => {}
+        :patriarch => {},
+        :matriarch => {},
+        :hero => {},
         :heroine_friends => []
       }}
 
